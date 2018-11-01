c79a824571aded160f221e312f7e6d5142fb61d5
jacobmalachowski <jacob.malachowski@55places.com>
2018-11-01 14:41:50 -0500

    rename bakery script

--- bin/bakery.php
+++
@@ -1,9 +0,0 @@
-<?php
-declare(strict_types=1);
-error_reporting(E_ALL);
-
-use Neighborhoods\Bakery\Baker;
-
-require_once __DIR__ . '/../vendor/autoload.php';
-$baker = new Baker();
-$baker->bake();
